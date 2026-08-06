57d8c7626f950dd591cf1ddf02dc863ed1ca8b92
jangidmayank629-max <jangidmayank629@gmail.com>
2026-08-06 12:10:57 +0530

    completing while looops

--- Python/python.questions/week 2.py
+++ Python/python.questions/week 2.py
@@ -1,76 +1,75 @@
 # # for loops 
 
 # #16-----------1
-# for i in range(16, 0, -1):
-#     print(i)
+for i in range(16, 0, -1):
+    print(i)
 
 # #20-----------50  
-# for i in range(20, 51, 1): 
-#     print(i) 
+for i in range(20, 51, 1): 
+    print(i) 
 
 # # -3 ---- -15
-# for i in range (-3,-16, -1):
-#     print(i)
+for i in range (-3,-16, -1):
+    print(i)
 
 # # table of any number 
-# a = int(input("Enter a number: "))
-# for i in range( a, (a*10)+1 , a):
-#     print(i)
+a = int(input("Enter a number: "))
+for i in range( a, (a*10)+1 , a):
+    print(i)
 
 # # printing a names letters
-# a = "ALPHABET"
-# for i in range(len(a)):
-#     print(a[i])
+a = "ALPHABET"
+for i in range(len(a)):
+    print(a[i])
 
 
 
 # Write a program to print all even numbers from 1 to 50 using a for loop.
 
-# for i in range(0, 51, 2):
-#     print(i)
+for i in range(0, 51, 2):
+    print(i)
 
 
 # Write a program to print numbers from 1 to 100, but use continue to skip all numbers that are divisible by 5.
-# for i in range(1, 101): 
-#     if i %5 == 0:
-#         continue
+for i in range(1, 101): 
+    if i %5 == 0:
+        continue
         
-#     print(i)
+    print(i)
 
 # Write a program to print numbers from 1 to 50, and use break to stop the loop when the number reaches 30.
 
-# for i in range(1, 51):
-#     if i == 30:
-#         break
-#     print(i)
+for i in range(1, 51):
+    if i == 30:
+        break
+    print(i)
 
 
 # Write a program to print all numbers from 1 to 100 that are not divisible by 3 using continue.
 
-# for i in range(1, 101):
-#     if i % 3 == 0:
-#         continue
-#     print(i)
+for i in range(1, 101):
+    if i % 3 == 0:
+        continue
+    print(i)
 
-# x = int(input("Enter the starting number: "))
-# for i in range(x, 101):
-#     if i % 3 == 0:
-#         continue
-#     print(i)
+x = int(input("Enter the starting number: "))
+for i in range(x, 101):
+    if i % 3 == 0:
+        continue
+    print(i)
 
 # Write a program to take a number  from the user and search for its first divisor other than  and itself. Use break when the divisor is found.
-# x = int(input("Enter a number: "))
-# for i in range(2, x):
-#     if x % i == 0:
-#       print(i)
-#       break
-# else :
-#        print("it is a prime number")
+
+for i in range(2, 101):
+    if 100 % i == 0:
+        print(i)
+        break
+
     
 
 # Write a program to print numbers from 1 to 100, but:
-# Skip multiples of 3 using continue
-# Stop completely when the number reaches 80 using break
+ # Skip multiples of 3 using continue
+ # Stop completely when the number reaches 80 using break
 
 for i in range (1, 101):
     if i % 3 == 0:
@@ -97,6 +96,66 @@ for i in range(1, 101):
 
 
 # Challenge: Write a program that prints all numbers from 1 to 100, but:
-# Skip numbers divisible by 2 using continue
-# Skip numbers divisible by 3 using continue
-# Stop the loop completely when the number reaches 75 using break
+ # Skip numbers divisible by 2 using continue
+ # Skip numbers divisible by 3 using continue
+ # Stop the loop completely when the number reaches 75 using break
+
+for i in range (100):
+  if i % 3 == 0 or i % 2 == 0:
+    continue
+  if i >= 75:
+    break
+  print(i)
+
+# while loop
+
+# printing 1 to 30
+
+a = 1
+while a <= 30:
+    print(a)
+    a += 1
+
+# 121 in 1 , 2 ,1  (palindrone or not )
+a = 121
+copy = a
+rev = 0
+while a > 0:
+    rev = rev * 10 + a % 10
+    a //= 10
+if copy == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+
+# mini number gussing game
+import random
+
+num = random.randint(1, 20)
+tries = 0
+guess = int(input("Guess a number between 1 and 20: "))
+
+while True:
+
+   if num == guess:
+    tries += 1
+    print("Congratulations! You guessed the number .")
+    break
+   
+   elif guess < num:
+    tries += 1
+    print("Your guess is too low. Try again.")
+
+   elif guess > num:
+    tries += 1
+    print("Your guess is too high. Try again.")
+    
+   else:
+    print("Invalid input. Please enter a number between 1 and 20.")
+    
+
+
+
+    
